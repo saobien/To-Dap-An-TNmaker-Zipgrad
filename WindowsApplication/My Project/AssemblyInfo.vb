070a23c8fa026e94a0114d0b510a4b7157e50187
MinhQuoc <saobien.me@gmail.com>
2023-07-01 15:04:48 +0700

    Add files via upload

---
+++ WindowsApplication/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TickZT")> 
+<Assembly: AssemblyDescription("Tô đáp án TNmaker và Zipgrade")> 
+<Assembly: AssemblyCompany("TQM")> 
+<Assembly: AssemblyProduct("TickZT")> 
+<Assembly: AssemblyCopyright("Copyright © TQM 2021")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9a97b13-e60e-4057-ac1c-1e78c9f237ab")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyFileVersion("1.0.0.1")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
